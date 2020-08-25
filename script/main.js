1170c48f2267f2e24f421850209f0d88878aa8d9
Nuno Martins <nunoormaker21@gmail.com>
2020-08-25 12:28:40 +0100

    Commit inicial

---
+++ script/main.js
@@ -0,0 +1,14 @@
+/**
+ * Como o script funciona?
+ *      1. liga servidor/websocket
+ *      2. (opcional) estabelece conexão com DB (Mongo/Firebase/MySQL)
+ *      3. começa o scrapping
+ *      4. (opcional) envia os dados para a DB
+ *      5. envia os dados por websocket ao cliente
+ */
+
+require('dotenv/config')
+
+class Script {
+
+}
